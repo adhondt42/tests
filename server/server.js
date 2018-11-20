a9b79ef6c5ce940c9cb206b5413f09baec522376
DOXE913 <adhondt@student.42.fr>
2018-11-20 12:03:57 +0100

    tests

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 let express = require('express')
 let app = express()
+let http = require('http')
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.get('/', (req, res) =>{
-    res.render('pages/index', {test: "testito"})
+let server = http.createServer()
+
+server.on('request', (request, response) => {
+    console.log("Il y a eu une requete")
+    response.writeHeader(200)
+    response.end('Salut, cava ?')
 })
 
+server.listen(8080)
+console.log("oki")
+// app.set('view engine', 'ejs')
+// app.use(express.static('public'))
+// app.get('/', (req, res) =>{
+//     res.render('pages/index', {test: "testito"})
+// })
 
-app.listen(8080)
+
+// app.listen(8080)  
